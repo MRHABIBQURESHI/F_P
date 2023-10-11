70c07e5120908b937a668d7b30f9debda05e46ab
mR . habIB qurEshi <105902699+MRHABIBQURESHI@users.noreply.github.com>
2023-10-11 23:03:56 +0500

    Create 3rd.dart

---
+++ 3rd.dart
@@ -0,0 +1,232 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(children: [
+        Container(
+          color: Colors.blue,
+          height: 200,
+          width: 200,
+          margin: EdgeInsets.all(30),
+        ),
+        Container(
+          color: Color.fromARGB(255, 99, 221, 69),
+          height: 200,
+          width: 200,
+          margin: EdgeInsets.all(30),
+        ),
+        Text("Text can not be Move"),
+        Container(
+          color: Colors.blue,
+          height: 200,
+          width: 200,
+          margin: EdgeInsets.all(30),
+        ),
+        Container(
+          color: Color.fromARGB(255, 99, 221, 69),
+          height: 200,
+          width: 200,
+          margin: EdgeInsets.all(30),
+        ),
+        Container(
+          color: Colors.blue,
+          height: 200,
+          width: 200,
+          margin: EdgeInsets.all(30),
+        ),
+        Container(
+          color: Color.fromARGB(255, 99, 221, 69),
+          height: 200,
+          width: 200,
+          margin: EdgeInsets.all(30),
+        ),
+        Container(
+          color: Colors.blue,
+          height: 200,
+          width: 200,
+          margin: EdgeInsets.all(30),
+        ),
+        Container(
+          color: Color.fromARGB(255, 99, 221, 69),
+          height: 200,
+          width: 200,
+          margin: EdgeInsets.all(30),
+        ),
+        Container(
+          color: Colors.blue,
+          height: 200,
+          width: 200,
+          margin: EdgeInsets.all(30),
+        ),
+        Container(
+          color: Color.fromARGB(255, 99, 221, 69),
+          height: 200,
+          width: 200,
+          margin: EdgeInsets.all(30),
+        ),
+        Container(
+          color: Colors.blue,
+          height: 200,
+          width: 200,
+          margin: EdgeInsets.all(30),
+        ),
+      ]),
+    ));
+
+    //     body: SingleChildScrollView(
+    //   scrollDirection: Axis.vertical,
+    //   child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+    //     SingleChildScrollView(
+    //       scrollDirection: Axis.horizontal,
+    //       child: Row(
+    //         // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //         mainAxisSize: MainAxisSize.min,
+    //         children: [
+    //           Container(
+    //             height: 300,
+    //             width: 300,
+    //             color: Colors.blue,
+    //             margin: EdgeInsets.all(40),
+    //           ),
+    //           Container(
+    //             height: 300,
+    //             width: 300,
+    //             color: Colors.red,
+    //             margin: EdgeInsets.all(40),
+    //           ),
+    //           Container(
+    //             height: 300,
+    //             width: 300,
+    //             color: Colors.blue,
+    //             margin: EdgeInsets.all(40),
+    //           ),
+    //           Container(
+    //             height: 300,
+    //             width: 300,
+    //             color: Colors.red,
+    //             margin: EdgeInsets.all(40),
+    //           ),
+    //           Container(
+    //             height: 300,
+    //             width: 300,
+    //             color: Colors.blue,
+    //             margin: EdgeInsets.all(40),
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //     Row(
+    //       // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //       children: [
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.blue,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.red,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.blue,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.red,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.blue,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //       ],
+    //     ),
+    //     Row(
+    //       // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //       children: [
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.blue,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.red,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.blue,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.red,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.blue,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //       ],
+    //     ),
+    //     Row(
+    //       // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //       children: [
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.blue,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.red,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.blue,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.red,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //         Container(
+    //           height: 300,
+    //           width: 300,
+    //           color: Colors.blue,
+    //           margin: EdgeInsets.all(40),
+    //         ),
+    //       ],
+    //     ),
+    //   ]),
+    // )
+  }
+}
